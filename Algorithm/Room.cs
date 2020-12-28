34dab7757b3d0818bc81a59dd2df257e4bebee6d
HaEun.Oh <haeun.oh@archipin.com>
2020-12-28 22:52:29 +0900

    linked list practice

---
+++ Algorithm/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithm
+{
+    class Room<T>
+    {
+        public T Data;
+        private Room<T> Next;
+        private Room<T> Prev;
+    }
+
+    class RoomList<T>
+    {
+        public int Count = 0;
+        
+    }
+}
