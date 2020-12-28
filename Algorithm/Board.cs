f580d579fa89f9fa801564c3984c9d73f19331ed
HaEun.Oh <haeun.oh@archipin.com>
2020-12-28 21:50:02 +0900

    Board start

---
+++ Algorithm/Board.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm
+{
+    class Board
+    {
+        public int[] _data = new int[25];
+        public LinkedList<int> _data3 = new LinkedList<int>();
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize();
+
+
+            Console.CursorVisible = false;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            const int WAIT_TICK = 1000 / 30; /*milisecond이기 때문에 1000 곱하기*/
+            const char CIRCLE = '\u25cf';
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+
+                #endregion
+
+                Console.SetCursorPosition(0, 0);
+
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.Write(CIRCLE);
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
