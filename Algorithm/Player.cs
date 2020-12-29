533b034a055f9654026bb1512ab62f185c4d898a
HaEun.Oh <haeun.oh@archipin.com>
2020-12-29 16:32:30 +0900

    changed to x, y order, started working on player

---
+++ Algorithm/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class Player
+    {
+        public int PosX { get; private set; }
+        public int PosY { get; private set; }
+        Random _random = new Random();
+
+        private Board _board;
+
+        public void Initialize(int posX, int posY, int destX, int destY, Board board)
+        {
+            PosX = posX;
+            PosY = posY;
+            
+            _board = board;
+        }
+
+        const int MOVE_TICK = 100;
+        private int _sumTick = 0;
+        public void Update(int deltaTick)
+        {
+            _sumTick += deltaTick;
+            if (_sumTick >= MOVE_TICK)
+            {
+                _sumTick = 0;
+                
+                // 0.1초마다 실행할 로직
+                int randValue = _random.Next(0, 5);
+                switch (randValue)
+                {
+                    case 0: // 상
+                        if (_board.Tile[PosX, PosY - 1] == Board.TileType.Empty)
+                            PosY = PosY - 1;
+                        break;
+                    case 1: // 하
+                        if (_board.Tile[PosX, PosY + 1] == Board.TileType.Empty)
+                            PosY = PosY + 1;
+                        break;
+                    case 2: // 좌
+                        if (_board.Tile[PosX - 1, PosY] == Board.TileType.Empty)
+                            PosX = PosX - 1;
+                        break;
+                    case 3: // 우
+                        if (_board.Tile[PosX + 1, PosY] == Board.TileType.Empty)
+                            PosX = PosX + 1; 
+                        break;
+                }
+            }
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -7,15 +7,14 @@ namespace Algorithm
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.Initialize();
-
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board.Size - 2, board.Size - 2, board);
 
             Console.CursorVisible = false;
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.ForegroundColor = ConsoleColor.Green;
 
             const int WAIT_TICK = 1000 / 30; /*milisecond이기 때문에 1000 곱하기*/
-            const char CIRCLE = '\u25cf';
             int lastTick = 0;
             while (true)
             {
@@ -24,21 +23,18 @@ namespace Algorithm
                 int currentTick = System.Environment.TickCount;
                 if (currentTick - lastTick < WAIT_TICK)
                     continue;
+                var deltaTick = currentTick - lastTick;
                 lastTick = currentTick;
 
                 #endregion
 
                 Console.SetCursorPosition(0, 0);
-
-                for (int i = 0; i < 25; i++)
-                {
-                    for (int j = 0; j < 25; j++)
-                    {
-                        Console.Write(CIRCLE);
-                        Console.Write(' ');
-                    }
-                    Console.WriteLine();
-                }
+                
+                // 로직
+                player.Update(deltaTick);
+                
+                // 렌더링
+                board.Render();
             }
         }
     }
